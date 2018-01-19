e537730f7d9925bed9b085f36d6cef791cd1a0fa
Guest User <Guest@Epicodus-7A.local>
2018-01-19 12:54:13 -0800

    Complete restart.  Made absolutely no progress.  Have abandonded hope, contemplating abandoning programming all together.  Have had no help, no resources, incomplete curriculum, overall dissatisfaction incredibly high, am currently learning nothing while my blood pressure approaches quadruple digits.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
-function beep(userArray) {
-    if(userArray.contains("0"))
-    { return alert("BEEEEEEP!");
+var number = prompt("Enter a number");{
+  if { number.includes("0");
+    return alert("BEEP!");
   } else {
-    return userNumber;
+    return alert("NA!")
   }
 }
-$(document).ready(function() {
-  $("form#userEntry").submit(function(event) {
-    event.preventDefault();
-    var userNumber = $("input#userNumber").val();
-    var userArray = Array.from(userNumber);
-    console.log(userArray);
-  });
-});
