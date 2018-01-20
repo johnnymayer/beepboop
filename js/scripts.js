ef741888fe7259823f5e5e48592a11a10e0fdbe5
Johnny Mayer <johnnymayer@gmail.com>
2018-01-20 13:29:44 -0800

    Upgrade to jquery-3.3.1, no other progress made.  Project is frustrating.  After 4+ hours this morning, I am giving up for the day.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
-    var number = $("input#number").val();
-    var numberMod = Array.from(number);
-    console.log(numberMod);
-
-    $(".number").text(number);
-
-    if (!result) {
-      $(".not").text("I'm sorry, Dave. I can't do that.");
-    } else {
-      $(".not").text("");
-    }
-
+    var userNumber = $("input#userNumber").val();
+    $(".userNumber").text(userNumber);
     $("#result").show();
   });
 });
