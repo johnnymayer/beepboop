b81c8eac5f0a716b884018eb5ace44e6ede957d3
johnnymayer <johnnymayer@gmail.com>
2018-01-21 20:41:41 -0800

    More updates, more frustration.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+//business logic
+var userNumberMod = function(userNumber) {
+  if (userNumber % 3) {
+    alert("I'm sorry, Dave. I can't do that.");
+  } else if (userNumber.matches(/[0]/)) {
+    alert("Beep!");
+  } else if (userNumber.matches(/[1]/)) {
+    alert("Boop!");
+  } else {
+    alert("Nada.");
+  }
+}
+
+//user interface logic
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
