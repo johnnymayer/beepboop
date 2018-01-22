124d9a8132845aa4ff1e98d3c53f6b7d1b695a42
Guest User <Guest@Epicodus-9D.local>
2018-01-22 13:20:00 -0800

    Add final update.  Project in working condition.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,42 @@
 //business logic
-var userNumberMod = function(userNumber) {
-  if (userNumber % 3) {
-    alert("I'm sorry, Dave. I can't do that.");
-  } else if (userNumber.matches(/[0]/)) {
-    alert("Beep!");
-  } else if (userNumber.matches(/[1]/)) {
-    alert("Boop!");
-  } else {
-    alert("Nada.");
-  }
-}
+var checkDivisible = function(userNumber) {
+    var isDivisible = false;
+  if (userNumber % 3 === 0) {
+    return true;
+  } return isDivisible;
+};
 
+var checkBeepBoop = function(userNumberArray) {
+    var isBeep = false;
+  for (var i = 0; i < userNumberArray.length; i++) {
+    if (userNumberArray[i] === "0") {
+      return true;
+    } else if (userNumberArray[i] === "1") {
+      return false;
+    }
+  }
+};
 //user interface logic
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
     var userNumber = $("input#userNumber").val();
-    $(".userNumber").text(userNumber);
-    $("#result").show();
+    var userNumberArray = Array.from(userNumber);
+    var divisible = checkDivisible(userNumber);
+    var isBeep = checkBeepBoop(userNumberArray);
+    console.log(divisible, isBeep);
+    if (divisible === true) {
+      $("#result").show();
+      $(".userNumberDisplay").text("I'm sorry, Dave. I can't do that.");
+    } else if (isBeep === true) {
+      $("#result").show();
+      $(".userNumberDisplay").text("BEEP");
+    } else if (isBeep === false) {
+      $("#result").show();
+      $(".userNumberDisplay").text("BOOP");
+    } else {
+      $("#result").show();
+      $(".userNumberDisplay").text(userNumber);
+    }
+    });
   });
-});
