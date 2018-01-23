dfea2bd52ccdd52bd050fab37b9523e2fc21ea62
johnnymayer <johnnymayer@gmail.com>
2018-01-22 20:44:49 -0800

    Two more hours of work.  Zero progress.  Cannot figure out array.  In tears.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,17 @@
 //business logic
-var checkDivisible = function(userNumber) {
-    var isDivisible = false;
-  if (userNumber % 3 === 0) {
-    return true;
-  } return isDivisible;
-};
-
-var checkBeepBoop = function(userNumberArray) {
-    var isBeep = false;
-  for (var i = 0; i < userNumberArray.length; i++) {
-    if (userNumberArray[i] === "0") {
-      return true;
-    } else if (userNumberArray[i] === "1") {
-      return false;
-    }
+function numberMod(inputtedUserNumber){
+var userNumberArray = [];
+for (i = 0; i <= userNumberArray.length; i++) {
+  userNumberArray.push(i);
   }
-};
+  console.log(userNumberArray);
+}
+
 //user interface logic
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
-    var userNumber = $("input#userNumber").val();
-    var userNumberArray = Array.from(userNumber);
-    var divisible = checkDivisible(userNumber);
-    var isBeep = checkBeepBoop(userNumberArray);
-    console.log(divisible, isBeep);
-    if (divisible === true) {
-      $("#result").show();
-      $(".userNumberDisplay").text("I'm sorry, Dave. I can't do that.");
-    } else if (isBeep === true) {
-      $("#result").show();
-      $(".userNumberDisplay").text("BEEP");
-    } else if (isBeep === false) {
-      $("#result").show();
-      $(".userNumberDisplay").text("BOOP");
-    } else {
-      $("#result").show();
-      $(".userNumberDisplay").text(userNumber);
-    }
-    });
+    var inputtedUserNumber = $("input#userNumberInput").val();
+    numberMod(inputtedUserNumber);
   });
+});
