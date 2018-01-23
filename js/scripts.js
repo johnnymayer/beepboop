16269fad91ac7eb7983ba9a8362457613fca5101
johnnymayer <johnnymayer@gmail.com>
2018-01-23 01:00:03 -0800

    Add JS functionality, numberArray.  Next step is convert 1s, 0s, and anything divisible by 3 to specific phrases.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 //business logic
-function numberMod(inputtedUserNumber){
-var userNumberArray = [];
-for (i = 0; i <= userNumberArray.length; i++) {
-  userNumberArray.push(i);
+var numberMod = function(numberArray){
+  if (numberArray % 3) {
+    $("#results").text("I'm sorry, Dave.");
+  } else if (numberArray.includes("0")) {
+    $("#results").text("beep");
+  } else if (numberArray.includes("1")) {
+    $("#results").text("boop");
+  } else {
+    $("#results").text(numberArray);
   }
-  console.log(userNumberArray);
-}
+};
 
 //user interface logic
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
     var inputtedUserNumber = $("input#userNumberInput").val();
+    var numberArray = [];
+      for(var i = 0; i <= inputtedUserNumber; ++i){
+        numberArray.push(i.toString());
+      }
+    $("#results").text(numberArray.join("-"));
     numberMod(inputtedUserNumber);
+    console.log(numberArray);
   });
 });
