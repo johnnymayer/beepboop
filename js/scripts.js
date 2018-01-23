a47dcbe76bc048fdc7ea83279ebac2a88018a0ab
johnny mayer <johnnymayer@gmail.com>
2018-01-23 12:56:55 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //business logic
-// var numberMod = function(numberArray){
-//   if (numberArray % 3) {
-//     $("#results").text("I'm sorry, Dave.");
-//   } else if (numberArray.includes("0")) {
-//     $("#results").text("beep");
-//   } else if (numberArray.includes("1")) {
-//     $("#results").text("boop");
-//   } else {
-//     $("#results").text(numberArray);
-//   }
-// };
+var numberMod = function(numberArray){
+  if (numberArray % 3 === 0) {
+    $("#results").text("I'm sorry, Dave.");
+  } else if (numberArray.includes("0")) {
+    $("#results").text("beep");
+  } else if (numberArray.includes("1")) {
+    $("#results").text("boop");
+  } else {
+    $("#results").text(numberArray);
+  }
+};
 
 //user interface logic
 $(document).ready(function() {
@@ -21,7 +21,7 @@ $(document).ready(function() {
         numberArray.push(i.toString());
       }
     $("#results").text(numberArray.join("-"));
-    numberMod(inputtedUserNumber);
+    numberMod(numberArray);
     console.log(numberArray);
   });
 });
